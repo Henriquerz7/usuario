1bdf1915ee6a4624165af23da608a2b041a94cb5
Henriquerz7 <henrique.r.zeri@gmail.com>
2026-02-12 15:15:52 -0300

    adicionado classes de security, entity e repository

---
+++ src/main/java/com/javanauta/infrastructure/entity/Usuario.java
@@ -0,0 +1,57 @@
+package com.javanauta.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", length = 100)
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "senha")
+    private String senha;
+
+    @OneToMany(cascade = CascadeType.ALL) // Em cascata quer dizer que quando excluir o usuário, o endereço e o email serão excluídos automaticamente
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id") // Na tabela endereço terá uma coluna usuario_id que irá identificar o usuário na tabela usuário
+    private List<Endereco> endereco; // Relaciona a tabela endereço
+
+    @OneToMany(cascade = CascadeType.ALL) // OneToMany neste caso é um usuário para vários telefones
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id") // usuario_id faz referencia a coluna id da tabela usuário
+    private List<Telefone> telefone;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
+
+
