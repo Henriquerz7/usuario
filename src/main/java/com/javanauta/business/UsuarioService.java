9326778254e13178227f0059a5abf6679e7e911b
Henriquerz7 <henrique.r.zeri@gmail.com>
2026-03-05 14:36:10 -0300

    inclusão de método put de usuário

--- src/main/java/com/javanauta/business/UsuarioService.java
+++ src/main/java/com/javanauta/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.javanauta.infrastructure.entity.Usuario;
 import com.javanauta.infrastructure.exception.ConflictException;
 import com.javanauta.infrastructure.exception.ResourceNotFoundException;
 import com.javanauta.infrastructure.repository.UsuarioRepository;
+import com.javanauta.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -48,5 +50,18 @@ public class UsuarioService {
 
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto) {
+        String email = jwtUtil.extractEmailToken(token.substring(7));             // Busca email do usuário através do token
+
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null); // Criptografa a senha caso o usuário tenha passado nova senha
+
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->       // Busca os dados do usuário no banco de dados
+                new ResourceNotFoundException("Email não localizado"));
+
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);                   // Mescla os dados recebidos na requisição (dto) com os dados do banco
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));  // Salva os dados do usuário convertido e pega o retorno e converte para UsuarioDTO
     }
 }
